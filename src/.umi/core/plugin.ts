e98cb1c1cf42a3c7b88416e0acd8a5d9f42630e5
AlamLR <alamlpez@gmail.com>
2024-01-04 16:55:44 -0600

    Cuerpo del proyecto

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'C:/Users/AlamLR/Desktop/proyecto/proyectoIS/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','locale','layout','layoutActionRef','request',],
+});
+
+export { plugin };
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from 'C:/Users/AlamLR/Desktop/proyecto/proyectoIS/node_modules/react-helmet';
